4315f5a04add45c0c9be09526c3ce8737fba3063
KieuTanTai <kieutantai2x@gmail.com>
2024-10-19 15:32:12 +0700

    config file Interface.js

--- Web-Books-Store/Javascript/Interface.js
+++ Web-Books-Store/Javascript/Interface.js
@@ -1,29 +1,38 @@
 'use strict'
-
 const query = document.querySelector.bind(document);
 const queryAll = document.querySelectorAll.bind(document);
 const fromId = document.getElementById.bind(document);
 const fromClass = document.getElementsByClassName.bind(document);
 const fromTag = document.getElementsByTagName.bind(document);
 // get Elements object 
-function getElementsHandler () {
+function getElementsHandler() {
      let getElements = {
-               getImages: () => queryAll(".product-image.js-item img"),
-               getSubMenuNav: () => query(".sub-menu-item.menu-nav"),
-               getJsLoginButton: () => queryAll(".lnw-btn.js-login"),
-               getJsSignupButton: () => queryAll(".lnw-btn.js-signup"),
-               getMainContainer: () => query("#main-container"),
-               getWebContent : () => query(".web-content")
+          getImages: () => queryAll(".product-image.js-item img"),
+          getSubMenuNav: () => query(".sub-menu-item.menu-nav"),
+          getJsLoginButton: () => queryAll(".lnw-btn.js-login"),
+          getJsSignupButton: () => queryAll(".lnw-btn.js-signup"),
+          getMainContainer: () => query("#main-container"),
+          getWebContent: () => query(".web-content")
      }
-     return getElements; 
+     return getElements;
 }
 
+// first params for checkActiveHTML would be added active class
+function checkActiveHTML(nameForm, ...restForm) {
+     restForm.forEach((form) => {
+          if (form.classList.contains("active"))
+               form.classList.remove("active");
+     });
+     if (!nameForm.classList.contains("active"))
+          nameForm.classList.add("active");
+     return nameForm;
+}
 
 // resize image
-function resizeImages (productImages, ratio) {
+function resizeImages(productImages, ratio) {
      productImages.forEach((image) => {
           image.addEventListener("load", () => {
-               image.style.height = ( ratio * image.offsetWidth) + "px";
+               image.style.height = (ratio * image.offsetWidth) + "px";
           });
      });
 
@@ -35,91 +44,84 @@ function resizeImages (productImages, ratio) {
 }
 
 // resize width of nav
-function resizeSmNav (subMenuNav) {
+function resizeSmNav(subMenuNav) {
      const childInner = subMenuNav.firstElementChild;
-     let parentWidth =  subMenuNav.offsetWidth;
+     let parentWidth = subMenuNav.offsetWidth;
      childInner.style.width = (parentWidth / 16) + "em";
 
-     window.addEventListener("resize", function() {
+     window.addEventListener("resize", function () {
           parentWidth = subMenuNav.offsetWidth;
           if (!childInner.width === "unset")
                childInner.style.width = "unset";
           else
                childInner.style.width = (parentWidth / 16) + "em";
-     }); 
+     });
 }
 
+// handle url
+
+
 // get promise DOM function
-async function getPromiseDOM (fileAddress) {
+async function promiseDOMHandler(fileAddress, elementsObj) {
      try {
           const response = await fetch(fileAddress);
-          if (!response.ok) {
-               throw (`your response status is : ${response.status} !`);
-          }
+          if (!response.ok)
+               throw new Error(`${response.status} (${response.statusText})`);
           const text = await response.text();
-          const parser = new DOMParser();
-          return parser.parseFromString(text, "text/html");
+          const newDOM = (new DOMParser()).parseFromString(text, "text/html");
+          accountEvents(elementsObj, newDOM);
      }
      catch (error) {
-          throw (`error when fetch your address! : ${error}`);
+          throw (`error when fetch your address! \n ${error}`);
      }
 }
 
 // function account
-function accountEvents (getElements, promiseAccount) {
-     const loginBtn = getElements.getJsLoginButton();
-     const signupBtn = getElements.getJsSignupButton();
+function accountEvents(elementsObj, promiseAccount) {
+     const loginBtn = elementsObj.getJsLoginButton();
+     const signupBtn = elementsObj.getJsSignupButton();
      loginBtn.forEach((button) => {
           button.addEventListener("click", () => {
-               accountDOMHandler(promiseAccount, button, getElements);
+               accountDOMHandler(promiseAccount, button, elementsObj);
           });
      });
 
      signupBtn.forEach((button) => {
           button.addEventListener("click", () => {
-               accountDOMHandler(promiseAccount, button, getElements);
+               accountDOMHandler(promiseAccount, button, elementsObj);
           });
      });
 }
 
-// first params for checkActiveHTML would be added active class
-function checkActiveHTML (nameForm, ...restForm) {
-     restForm.forEach((form) => {
-          if (form.classList.contains("active"))
-               form.classList.remove("active");
-     });
-     if (!nameForm.classList.contains("active"))
-          nameForm.classList.add("active");
-     return nameForm;
-}
-async function accountDOMHandler (promiseDOM, jsBtn, getElements) {
+async function accountDOMHandler(promiseDOM, jsBtn, elementsObj) {
      try {
           const accountDocs = await promiseDOM;
-          const accountContent = await accountDocs.getElementById("main-content");
-          let loginHTML = await accountDocs.getElementById("login");               
+          let loginHTML = await accountDocs.getElementById("login");
           let signupHTML = await accountDocs.getElementById("sign-up");
+          const accountTitle = await accountDocs.querySelector("title");
+          const accountContent = await accountDocs.getElementById("main-content");
           let forgotPassHTML = await accountDocs.getElementById("forgot-password");
-          const accountTitle = accountDocs.querySelector("title");
-          const webContent = getElements.getWebContent();
-          let placeInsert = Array.from(getElements.getMainContainer().children).find((element) => element.id === "main-content");
+          let placeInsert = Array.from(elementsObj.getMainContainer().children).find((element) => element.id === "main-content");
+          const webContent = elementsObj.getWebContent();
 
-          if (placeInsert !== undefined) {
-               if (jsBtn.classList.contains("js-login")) {
-                    accountTitle.innerText = "Đăng Nhập";
-                    query("Title").innerText = accountTitle.innerText;
-                    webContent.scrollIntoView({behavior: "instant", block: "start", inline: "nearest"});
-                    // check if login form have active or not
-                    loginHTML = checkActiveHTML(loginHTML, signupHTML, forgotPassHTML);
-                    placeInsert.innerHTML = accountContent.innerHTML;
-               }
-               else if (jsBtn.classList.contains("js-signup")) {
-                    accountTitle.innerText = "Đăng Ký";
-                    query("Title").innerText = accountTitle.innerText;
-                    webContent.scrollIntoView({behavior: "instant", block: "start", inline: "nearest"});
-                    // check if signup form have active or not
-                    signupHTML = checkActiveHTML(signupHTML, loginHTML, forgotPassHTML);
-                    placeInsert.innerHTML = accountContent.innerHTML;
-               }
+          if (placeInsert === undefined)
+               throw new Error(`your place you wanna insert is ${placeInsert}!`)
+
+          if (jsBtn.classList.contains("js-login")) {
+               accountTitle.innerText = "Đăng Nhập";
+               query("Title").innerText = accountTitle.innerText;
+               webContent.scrollIntoView({ behavior: "instant", block: "start", inline: "nearest" });
+               // check if login form have active or not
+               loginHTML = checkActiveHTML(loginHTML, signupHTML, forgotPassHTML);
+               placeInsert.innerHTML = accountContent.innerHTML;
+          }
+          else if (jsBtn.classList.contains("js-signup")) {
+               accountTitle.innerText = "Đăng Ký";
+               query("Title").innerText = accountTitle.innerText;
+               webContent.scrollIntoView({ behavior: "instant", block: "start", inline: "nearest" });
+               // check if signup form have active or not
+               signupHTML = checkActiveHTML(signupHTML, loginHTML, forgotPassHTML);
+               placeInsert.innerHTML = accountContent.innerHTML;
           }
      }
      catch (error) {
@@ -127,11 +129,10 @@ async function accountDOMHandler (promiseDOM, jsBtn, getElements) {
      }
 }
 
-document.addEventListener ("DOMContentLoaded", function() {
-     let ratio = 9/6;
-     let getElements = getElementsHandler();
-     let promiseAccountDOM = getPromiseDOM("../HTML/account.html"); 
-     resizeImages(getElements.getImages(), ratio);
-     resizeSmNav(getElements.getSubMenuNav());
-     accountEvents(getElements, promiseAccountDOM);
+document.addEventListener("DOMContentLoaded", function () {
+     const ratio = 9 / 6;
+     let elementsObj = getElementsHandler();
+     resizeSmNav(elementsObj.getSubMenuNav());
+     resizeImages(elementsObj.getImages(), ratio);
+     promiseDOMHandler("../HTML/account.html", elementsObj);
 })
