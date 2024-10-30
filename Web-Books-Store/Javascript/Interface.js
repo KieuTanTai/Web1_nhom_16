c55829021e5ead2b81f96baa5d2f62b316d924c6
KieuTanTai <kieutantai2x@gmail.com>
2024-10-30 07:22:40 +0700

    fix bug state too much navigation, add function throttling on Bridge.js

--- Web-Books-Store/Javascript/Interface.js
+++ Web-Books-Store/Javascript/Interface.js
@@ -3,6 +3,7 @@ import * as bridge from "./Bridge.js";
 
 function formatPrices (elementsObj) {
      const pricesContainer = elementsObj.getElementPrices();
+
      if (pricesContainer) {
           const formatPricesHandler = new Intl.NumberFormat("vi-VN", {style: "currency" ,currency: "VND", minimumSignificantDigits: "3"});
           pricesContainer.forEach ((element) => {
@@ -15,11 +16,12 @@ function formatPrices (elementsObj) {
 function setTimeFS (elementsObj) {
      let fSTime = elementsObj.getTimeFS(); 
      if (fSTime) {
-          let testDate = new Date();
+          let date = new Date();
           let stringTime = localStorage.getItem("flashSaleTime"); 
           fSTime = Array.from(elementsObj.getTimeFS().children);
+
           if (!stringTime)
-               localStorage.setItem("flashSaleTime", testDate.toLocaleTimeString("vi-VN"));
+               localStorage.setItem("flashSaleTime", date.toLocaleTimeString("it-IT"));
           else {
                let index = 0;
                let timeArray = [];
@@ -39,61 +41,62 @@ function setTimeFS (elementsObj) {
 
 async function startCountDown (timeArray, elementsObj) {
      try {
-          window.addEventListener ("beforeunload", (e) => {
+          window.addEventListener ("beforeunload", () => {
                localStorage.setItem("flashSaleTime", `${timeArray[0].innerText}:${timeArray[1].innerText}:${timeArray[2].innerText}`);
           }) ;
 
-          if (timeArray[2].innerText !== "0" ) {
+          if (timeArray[2].innerText !== "00" )
                timeArray[2].innerText = await countDown(timeArray[2], "seconds");
-          }
 
-          if (timeArray[2].innerText === "0" && timeArray[1].innerText !== "0") {
+          if (timeArray[2].innerText === "00" && timeArray[1].innerText !== "00") {
                timeArray[2].innerText = "60";
-               timeArray[1].innerText = await countDown (timeArray[1], "minutes");
+               timeArray[1].innerText = await countDown(timeArray[1], "minutes");
           }
-          else if (timeArray[2].innerText === "0" && timeArray[1].innerText === "0" && timeArray[0].innerText !== "0") {
+          else if (timeArray[2].innerText === "00" && timeArray[1].innerText === "00" && timeArray[0].innerText !== "00") {
                timeArray[1].innerText = "60";
-               timeArray[0].innerText = await countDown (timeArray[0], "minutes");
+               timeArray[0].innerText = await countDown(timeArray[0], "minutes");
           }
 
-          if (timeArray[2].innerText !== "0" || timeArray[1].innerText !== "0" || timeArray[0].innerText !== "0")
-               startCountDown (timeArray);
+          if (timeArray[2].innerText !== "00" || timeArray[1].innerText !== "00" || timeArray[0].innerText !== "00")
+               startCountDown(timeArray);
           else {
                const fSTable = elementsObj.getFSTable(); 
                if (!fSTable) {
                     alert("not found flash sale product!");
-                    return;
+                    return false;
                }
                fSTable.classList.add("disable");
           }
-     } catch (error) {
+     } 
+     catch (error) {
           alert(error);               
      }
 }
 
 function countDown (timeHandler, typeTime) {
-     if (typeTime === "seconds")
-          return new Promise ((resolve, reject) => {
-               if (timeHandler <= 0)
-                    reject(timeHandler.innerText);
+     return new Promise ((resolve, reject) => {
+          if (!timeHandler || isNaN(parseInt(timeHandler.innerText)) || parseInt(timeHandler.innerText) <= 0)
+               reject(new Error("invalid time!"));
+          const timeLength = timeHandler.innerText;
 
-               const timeLength = timeHandler.innerText;
+          if (typeTime === "seconds") {
                for (let i = 0; i < timeLength; i++) {
                     setTimeout (()=> {
-                         timeHandler.innerText-- ;
-                         if (timeHandler.innerText === "0")
+                         let currentTime = parseInt(timeHandler.innerText);
+                         timeHandler.innerText = (--currentTime).toString().padStart(2, '0');
+                         if (timeHandler.innerText === "00")
                               resolve(timeHandler.innerText);
                     }, 1000 * i);
                }
-          });
-
-     if (typeTime === "minutes" || typeTime === "hours")
-          return new Promise ((resolve, reject) => {
-               if (timeHandler <= 0)
-                    reject(timeHandler.innerText);
-               timeHandler.innerText-- ;
+          }
+          else if (typeTime === "minutes" || typeTime === "hours") {
+               let currentTime = parseInt(timeHandler.innerText);
+               timeHandler.innerText = (--currentTime).toString().padStart(2, '0');
                resolve(timeHandler.innerText);
-          });
+          }
+          else
+               reject(new Error("error time!"));
+     });
 }
 
 // fix bug interface function
@@ -102,7 +105,7 @@ function resizeImages (elementsObj, ratio) {
      const productImages = elementsObj.getImages();
      if (!productImages) {
           alert("not found any image!");
-          return;
+          return false;
      }
 
      productImages.forEach((image) => {
@@ -123,9 +126,8 @@ function resizeSmNav (elementsObj) {
      const subMenuNav = elementsObj.getSubMenuNav();
      if (!subMenuNav) {
           alert ("not found nav!");
-          return;
+          return false;
      }
-
      const childInner = subMenuNav.firstElementChild;
      let parentWidth = subMenuNav.offsetWidth;
      childInner.style.width = (parentWidth / 16) + "em";
@@ -172,7 +174,6 @@ async function addDOMFooter (elementsObj) {
 document.addEventListener ("DOMContentLoaded", function () {
      const ratio = 9 / 6;
      const elementsObj = bridge.default();
-
      //create check DOM of Header and Footer
      addDOMHeader(elementsObj);
      addDOMFooter(elementsObj);    
