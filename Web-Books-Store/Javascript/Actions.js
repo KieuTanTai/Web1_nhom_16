10c1802d76eb331774a074207af02bdbf92ecdd0
KieuTanTai <kieutantai2x@gmail.com>
2024-11-16 07:06:46 +0700

    fix bug navigate btn

--- Web-Books-Store/Javascript/Actions.js
+++ Web-Books-Store/Javascript/Actions.js
@@ -84,8 +84,10 @@ function behaviorSlides(nameSlide, showCount, haveDots) {
      container = Array.from(container);
 
      // execute for dots and others if not found
-     while(parent !== bridge.$("#main-content"))
+     let limitParent = bridge.$("#main-content");
+     while(parent !== limitParent && parent.parentElement !== limitParent) {
           parent = parent.parentElement;
+     }
      // first call init for create dot
      fInterface.createDots(parent, showCount);
 
@@ -97,6 +99,9 @@ function behaviorSlides(nameSlide, showCount, haveDots) {
 
           if (!nextButtons)
                nextButtons = parent.querySelector(".next-btn");
+
+          console.log(prevButtons);
+          console.log(nextButtons);
      }
 
      if (nextButtons) {
