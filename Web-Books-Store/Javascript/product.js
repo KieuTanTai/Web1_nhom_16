c3b3a339518d7b72de0b052a715fb0f22058b166
MKha546 <kha.nguyenminh167@gmail.com>
2024-12-01 16:19:35 +0700

    merge nhanh

---
+++ Web-Books-Store/Javascript/product.js
@@ -0,0 +1,19 @@
+
+var bookList = [
+    {
+      masp: "001",
+      tensp: "Khẽ Hát Lời Yêu - 1 ",
+      loaisach: "Manga",
+      hinh: "./Assets/Images/Books/Manga/khe_hat_loi_yeu_1.webp",
+      gia: "55.250",
+      
+    },
+    {
+      masp: "002",
+      tensp: "Khẽ Hát Lời Yêu - 2",
+      thuonghieu: "Whisky",
+      hinh: "./Assets/Images/Books/Manga/khe_hat_loi_yeu_2.webp",
+      gia: "55.250",
+    
+    },
+];
